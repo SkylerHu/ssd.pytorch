4014811f025fece9f2d64e1679c2758a17d980b0
hu.leping <hu.leping@immomo.com>
2020-01-09 20:12:41 +0800

    fix

--- eval.py
+++ eval.py
@@ -294,7 +294,7 @@ cachedir: Directory for caching the annotations
     class_recs = {}
     npos = 0
     for imagename in imagenames:
-        R = [obj for obj in recs[imagename] if obj['name'] == classname]
+        R = [obj for obj in recs.get(imagename, {}) if obj.get('name') == classname]
         bbox = np.array([x['bbox'] for x in R])
         difficult = np.array([x['difficult'] for x in R]).astype(np.bool)
         det = [False] * len(R)
